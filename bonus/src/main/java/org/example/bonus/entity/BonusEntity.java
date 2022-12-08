9a6a44844c1cc218a39fceaaa891648d0262317b
Igor Zasypkin <zas.i.e@yandex.ru>
2022-12-08 17:35:58 +0300

    added entity

---
+++ bonus/src/main/java/org/example/bonus/entity/BonusEntity.java
@@ -0,0 +1,46 @@
+package org.example.bonus.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "bonuses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BonusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "user_id"))
+    @AttributeOverride(name = "name", column = @Column(name = "user_name"))
+    private UserEmbedded user;
+
+
+    private long orderId;
+
+    private int bonusSum;
+
+    private Instant created;
+
+    private Instant validTo;
+
+    @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class UserEmbedded {
+        private long id;
+        private String name;
+    }
+}
---
+++ id/src/main/java/org/example/id/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package org.example.id.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false, columnDefinition = "TEXT")
+    private String name;
+    @Column(unique = true, nullable = false, columnDefinition = "TEXT")
+    private String login;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String password;
+    @ElementCollection
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false, columnDefinition = "TEXT")
+    private List<String> roles;
+    @Column(name = "station_id", nullable = false)
+    private long stationId;
+}
