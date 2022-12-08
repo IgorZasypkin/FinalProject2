8fd8a42c920ebbce74e1af208c2dbce3a4e28654
Igor Zasypkin <zas.i.e@yandex.ru>
2022-12-08 17:35:25 +0300

    генерация токенов

---
+++ id/src/main/java/org/example/id/entity/TokenEntity.java
@@ -0,0 +1,45 @@
+package org.example.id.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Builder // lombok
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity // аннотации hibernate
+@Table(name = "tokens")
+public class TokenEntity {
+    @Id
+    private String token;
+
+    @ManyToOne(optional = false)
+    private UserEntity user;
+
+    @Column( // поле представленнное колонкой в бд
+            insertable = true,
+            updatable = false,
+            nullable = false,
+            columnDefinition = "TIMESTAMPTZ NOT NULL"
+    )
+    private Instant expire;
+
+    @Column(
+            insertable = false,
+            updatable = false,
+            nullable = false,
+            columnDefinition = "TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP"
+    )
+    private Instant created;
+}
---
+++ id/src/main/java/org/example/id/security/TokenGenerator.java
@@ -0,0 +1,13 @@
+package org.example.id.security;
+
+import org.springframework.security.crypto.keygen.Base64StringKeyGenerator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenGenerator {
+    private final Base64StringKeyGenerator generator = new Base64StringKeyGenerator(60);
+
+    public String generate() {
+        return generator.generateKey();
+    }
+} //раздать коллегам токены,что бы они их добавили к своим базам, а они прописываю  вапликейшн пропертиес, в своих проектах
