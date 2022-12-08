7232ace245f721075dc1ad5ee314d2345e73041c
Igor Zasypkin <zas.i.e@yandex.ru>
2022-12-08 17:30:43 +0300

    Делаем контроллер для bonus

---
+++ bonus/src/main/java/org/example/bonus/error/ApplicationControllerAdvice.java
@@ -0,0 +1,37 @@
+package org.example.bonus.error;
+
+import org.example.bonus.exception.ForbiddenException;
+import org.example.bonus.exception.BonusNotFoundException;
+import org.example.bonus.exception.ServiceAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponseDTO handle(final MethodArgumentNotValidException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.ARGUMENT_NOT_VALID);
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponseDTO handle(final ForbiddenException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.EXECUTE_ACCESS_FORBIDDEN);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponseDTO handle(final ServiceAlreadyExistsException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.SERVICE_ALREADY_EXISTS);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handle(final BonusNotFoundException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.THE_ORDER_CANNOT_FIND_THE_DATA);
+    }
+}
